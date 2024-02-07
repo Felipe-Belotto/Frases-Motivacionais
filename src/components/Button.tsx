1b9ec50dd136bc0ee4a30f0dab3cc96c92da81d9
Felipe-Belotto <felipeeduardo817@gmail.com>
2024-02-07 18:07:33 -0300

    adicionando shad cn para poder integrar estilos tailwind do componente Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,15 @@
 import { ComponentPropsWithRef, forwardRef } from "react";
+import { cn } from "../lib/util";
 
 type ButtonProps = ComponentPropsWithRef<"button">
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button ({children, ...props}: ButtonProps, ref) {
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(function Button ({children,className, ...props}: ButtonProps, ref) {
 
   return (
     <button
     {...props}
     ref={ref}
-    className=" px-4 py-2 font-bold text-white bg-transparent bg-gray-800 border-2 border-white rounded bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50"
+    className={cn("px-4 py-2 font-bold text-white bg-transparent bg-gray-800 border-2 border-white rounded bg-opacity-10 focus:outline-none focus:ring-2 focus:ring-white focus:ring-opacity-50", className)}
   >
     {children}
   </button>
